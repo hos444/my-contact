919eaad7aacfc7e2fce03431dbaae4675913a16f
hossammustafa11 <hm20220214@sva.edu.eg>
2025-12-02 21:22:28 +0200

    Update:the final broject

--- lib/my_contacts.dart
+++ lib/my_contacts.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:my_conact/social_media_icon.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
 class Mycontacts extends StatelessWidget {
   Mycontacts({super.key});
-  List<String> socialmedis = [
-    'ins.jpg',
-    'lenked.avif',
-    'sm-icons-facebook-logo.webp',
-    'tektok.png',
-    'watsapp.png',
-    'you.png',
-    'camp.jpg',
-    'tell.webp',
-   
-  ];
+
+  Map<String, String> socialMedia = {
+    'tektok.png': 'https://www.tiktok.com/@_eng_hossam?_r=1&_t=ZS-91rIt8ppE1w',
+    'watsapp.png': 'https://wa.me/+201144112445',
+    'ins.jpg':
+        'https://www.instagram.com/10100110h?igsh=MXNxcWRvcmhxZXZkaQ%3D%3D&utm_source=qr',
+    'sm-icons-facebook-logo.webp':
+        'https://www.facebook.com/hossam.moustafa.9678',
+    'you.png':
+        'https://youtube.com/channel/UCPw7_yhXjw_8inx5rdJo4KA?si=nTdaldRMKeS2oRJI',
+    'camp.jpg': 'https://www.datacamp.com/portfolio/hoszblak',
+    'tell.webp': 'https://t.me/+201144112445',
+    'lenked.avif': 'https://www.linkedin.com/in/hossam-mustafa-0b7450260/',
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Column(
@@ -26,11 +30,11 @@ class Mycontacts extends StatelessWidget {
           children: [
             SizedBox(width: double.infinity, height: 40),
             CircleAvatar(
-              backgroundImage: AssetImage('assets/images.png'),
+              backgroundImage: AssetImage('assets/hos.jpg'),
               radius: 130,
             ),
             Text(
-              'Call Us',
+              'Call my',
               style: TextStyle(
                 color: Colors.blue,
                 fontSize: 30,
@@ -39,35 +43,38 @@ class Mycontacts extends StatelessWidget {
               ),
             ),
             SizedBox(width: double.infinity),
-        
+
             Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text(
-                  '01144112445',
+                  '+201144112445',
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
-        
+
                     color: Colors.grey,
                   ),
                 ),
                 IconButton(
                   icon: Icon(Icons.phone, size: 30, color: Colors.grey),
                   onPressed: () {
-                    print('icon is pressed');
+                    launchUrl(Uri.parse('tel:+201144112445'));
                   },
                 ),
               ],
             ),
             SizedBox(height: 20),
             GridView.builder(
-              itemCount: socialmedis.length,
+              itemCount: socialMedia.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
               ),
               itemBuilder: (context, index) {
-                return SocialMediaIcon(socialMedia: socialmedis[index]);
+                return SocialMediaIcon(
+                  socialMedia: socialMedia.keys.toList()[index],
+                  socialMediaLink: socialMedia.values.toList()[index],
+                );
               },
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
--- lib/social_media_icon.dart
+++ lib/social_media_icon.dart
@@ -1,20 +1,27 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaIcon extends StatelessWidget {
   final String socialMedia;
-  const SocialMediaIcon({super.key, required this.socialMedia});
-
+  final String socialMediaLink;
+  const SocialMediaIcon({super.key,
+  required this.socialMedia,
+required this.socialMediaLink,
+  });
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: CircleAvatar(
-        backgroundColor: Colors.white,
-        backgroundImage: AssetImage('assets/$socialMedia'),
-        radius: 50,
+      child: InkWell(
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage('assets/$socialMedia'),
+          radius: 50,
+        ),
+        onTap: () {
+        launchUrl(Uri.parse(socialMediaLink),mode:LaunchMode.externalApplication );
+        },
       ),
     );
   }
